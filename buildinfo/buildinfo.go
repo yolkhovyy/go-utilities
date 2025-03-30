9b7aee680b69fa301f92c8ddfee8f1cb1e6d07f2
Yuriy Olkhovyy <y.olkhovyy@gmail.com>
2025-03-30 09:10:01 +0200

    Remove Version

--- buildinfo/buildinfo.go
+++ buildinfo/buildinfo.go
@@ -5,34 +5,29 @@ import (
 )
 
 type Data struct {
-	Commit  string
-	Time    string
-	Version string
+	Revision string
+	Time     string
 }
 
 const unknown = "unknown"
 
-//nolint:gochecknoglobals
-var Version string
-
 // ReadData extracts build metadata.
 func ReadData() Data {
 	info := Data{
-		Commit:  unknown,
-		Time:    unknown,
-		Version: Version,
+		Revision: unknown,
+		Time:     unknown,
 	}
 
 	if buildInfo, ok := debug.ReadBuildInfo(); ok {
 		for _, setting := range buildInfo.Settings {
 			switch setting.Key {
 			case vcsKeyRevision:
-				info.Commit = setting.Value
+				info.Revision = setting.Value
 			case vcsKeyTime:
 				info.Time = setting.Value
 			case vcsKeyModified:
 				if setting.Value == "true" {
-					info.Commit += " (modified)"
+					info.Revision += " (modified)"
 				}
 			}
 		}
